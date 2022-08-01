d0d432e0f1d28615e2b787c892b8620ce2b4c8f4
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-08-01 21:27:23 +0530

    Add parallel_dl.sh

---
+++ scripts/parallel_dl.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Read url from stdin
+url=$1
+
+# download with curl five at a time using parallel command
+echo $url | parallel -j5 curl -O -L --retry 5 --retry-max-time 120 {}
+
