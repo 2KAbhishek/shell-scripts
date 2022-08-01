698827f3de240fb888102ee9920a1af86a285bda
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-08-01 21:36:59 +0530

    Fix glob splitting

--- scripts/parallel_dl.sh
+++ scripts/parallel_dl.sh
@@ -4,5 +4,5 @@
 url=$1
 
 # download with curl five at a time using parallel command
-echo $url | parallel -j5 curl -O -L --retry 5 --retry-max-time 120 {}
+echo "$url" | parallel -j5 curl -O -L --retry 5 --retry-max-time 120 {}
 
